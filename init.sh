1bef350c0697866588423ab605f00712c6de648b
Asaki <37176460+yudingsxt@users.noreply.github.com>
2025-01-02 22:24:06 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -40,6 +40,40 @@ sysctl -p
 # 更新sysctl.conf文件
 echo "Writing to /etc/sysctl.conf..."
 cat <<EOT >> /etc/sysctl.conf
+fs.file-max=1000000
+fs.inotify.max_user_instances=65536
+
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+net.ipv4.ip_local_port_range = 2000 65535
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.lo.forwarding = 1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_retries1=3
+net.ipv4.tcp_retries2=5
+net.ipv4.tcp_orphan_retries=3
+net.ipv4.tcp_syn_retries=3
+net.ipv4.tcp_synack_retries=3
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_fin_timeout=10
+net.ipv4.tcp_max_tw_buckets=10000
+net.ipv4.tcp_max_syn_backlog=131072
+net.core.netdev_max_backlog=131072
+net.core.somaxconn=32768
+net.ipv4.tcp_keepalive_time=300
+net.ipv4.tcp_keepalive_probes=3
+net.ipv4.tcp_keepalive_intvl=30
+net.ipv4.tcp_fastopen=3
+net.ipv4.tcp_autocorking=0
+net.ipv4.tcp_slow_start_after_idle=0
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
 net.ipv4.tcp_frto=0
@@ -48,16 +82,21 @@ net.ipv4.tcp_rfc1337=0
 net.ipv4.tcp_sack=1
 net.ipv4.tcp_fack=1
 net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_adv_win_scale=-2
 net.ipv4.tcp_moderate_rcvbuf=1
-net.core.rmem_max=33554432
-net.core.wmem_max=33554432
-net.ipv4.tcp_rmem=4096 87380 33554432
-net.ipv4.tcp_wmem=4096 16384 33554432
+net.core.rmem_max=335544320
+net.core.wmem_max=335544320
+net.ipv4.tcp_rmem=8192 262144 536870912
+net.ipv4.tcp_wmem=4096 16384 536870912
+net.ipv4.tcp_collapse_max_bytes = 6291456
+net.ipv4.tcp_notsent_lowat = 131072
 net.ipv4.udp_rmem_min=8192
 net.ipv4.udp_wmem_min=8192
-net.core.default_qdisc=fq
+net.ipv4.tcp_mem=262144 1048576 4194304
+net.ipv4.udp_mem=262144 1048576 4194304
 net.ipv4.tcp_congestion_control=bbr
+net.core.default_qdisc=fq
+net.ipv4.ping_group_range=0 2147483647
 EOT
 
 # 加载新的网络配置
